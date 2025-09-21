b4a6a6c24f3c287be881885027318966ff358a6c
bluesentinelsec <bluesentinel@protonmail.com>
2025-09-21 16:05:07 -0400

    added test tilemap

---
+++ resources/tilesets/test-tileset.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="test-tileset" tilewidth="32" tileheight="32" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../images/dirt_32x32.png" width="32" height="32"/>
+ </tile>
+ <tile id="1">
+  <image source="../images/tree_32x32.png" width="32" height="32"/>
+ </tile>
+ <tile id="2">
+  <image source="../images/enemy_32x32.png" width="32" height="32"/>
+ </tile>
+ <tile id="3">
+  <image source="../images/hero_32x32.png" width="32" height="32"/>
+ </tile>
+</tileset>
